3565941825d570a7461938e655f37da7a4284ccf
Prashant Khanal <prashantkhanal32@gmail.com>
2024-05-15 00:27:46 +0545

    fix: video play

--- src/components/video-player/index.tsx
+++ src/components/video-player/index.tsx
@@ -11,16 +11,26 @@ interface IProps {
 
 const VideoPlayer: React.FC<IProps> = ({ url, handleEndVideo }) => {
   const [loading, setLoading] = React.useState(true);
-  const [isPaused, setIsPaused] = React.useState(false); // Autoplay by default
-  const [showControls, setShowControls] = React.useState(false);
+  const [isPaused, setIsPaused] = React.useState(true);
+  const [showControls, setShowControls] = React.useState(true);
 
-  const handleControls = () => {
+  const handleControls: () => void = (): void => {
     setShowControls(true);
   };
+  const handlePlayPause: () => void = (): void => {
+    if (isPaused) {
+      setIsPaused(false);
+      setShowControls(true);
+      setTimeout((): void => setShowControls(false), 2000);
 
-  const handlePlayPause = () => {
-    setIsPaused((prevPaused) => !prevPaused);
-    setShowControls(true);
+      return;
+    }
+    setTimeout((): void => setShowControls(false), 2000);
+    setIsPaused(true);
+  };
+  const handlePlay: () => void = (): void => {
+    setTimeout((): void => setShowControls(false), 500);
+    setIsPaused(true);
   };
 
   const handleBuffer = () => setLoading(true);
@@ -41,26 +51,28 @@ const VideoPlayer: React.FC<IProps> = ({ url, handleEndVideo }) => {
         onBufferEnd={handleBufferEnd}
         className="react-player"
         onEnded={handleEndVideo}
-        config={{
-          file: {
-            attributes: {
-              autoPlay: true,
-              muted: false,
-            },
-          },
-        }}
         style={{ paddingTop: '100%', width: '100%', maxWidth: '100%' }}
       />
-      {showControls && !loading && (
+      {showControls && (
         <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#000000b5] rounded-lg">
           <div className="absolute z-50 top-[45%] left-[45%]">
-            <button onClick={handlePlayPause}>
-              <img
-                src={isPaused ? '/play.png' : '/pause.png'} // Toggle play/pause icon
-                alt="play-button"
-                className="w-16 h-16 object-cover"
-              />
-            </button>
+            {isPaused ? (
+              <button onClick={handlePlayPause}>
+                <img
+                  src="/play.png"
+                  alt="play-button"
+                  className="w-16 h-16 object-cover"
+                />
+              </button>
+            ) : (
+              <button onClick={handlePlay}>
+                <img
+                  src="/pause.png"
+                  alt="play-button"
+                  className="w-16 h-16 object-cover"
+                />
+              </button>
+            )}
           </div>
         </div>
       )}
