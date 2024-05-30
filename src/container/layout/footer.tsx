901661c12801a6b6661e7d96732fa6a0e804406b
Prashant Khanal <prashantkhanal32@gmail.com>
2024-05-30 17:08:27 +0545

    fix: height of the screen

--- src/container/layout/footer.tsx
+++ src/container/layout/footer.tsx
@@ -5,7 +5,7 @@ interface IProps {
 }
 
 const Footer: React.FC<IProps> = ({ children }) => (
-  <div className="fixed -bottom-5 left-0 right-0 mx-auto w-full bg-white px-5 pb-10 sm:w-[480px] md:w-[480px] lg:w-[480px] xl:w-[480px] 2xl:w-[480px]">
+  <div className=" -bottom-5 left-0 right-0 mx-auto w-full bg-white px-5 pb-10 sm:w-[480px] md:w-[480px] lg:w-[480px] xl:w-[480px] 2xl:w-[480px]">
     {children}
   </div>
 );
