119996d7e1328206e784751428cf568488cef4ab
Prashant Khanal <prashantkhanal32@gmail.com>
2024-06-03 19:06:53 +0545

    fix: video preview

--- src/container/layout/footer.tsx
+++ src/container/layout/footer.tsx
@@ -5,9 +5,7 @@ interface IProps {
 }
 
 const Footer: React.FC<IProps> = ({ children }) => (
-  <div className="fixed bottom-4 left-0 right-0 mx-auto w-full bg-white px-4 sm:w-[480px] md:w-[480px] lg:w-[480px] xl:w-[480px] 2xl:w-[480px]">
-    {children}
-  </div>
+  <div className="mt-4 w-full bg-white">{children}</div>
 );
 
 export default React.memo(Footer);
