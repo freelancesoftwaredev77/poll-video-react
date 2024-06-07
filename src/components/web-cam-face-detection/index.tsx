4b319bf674156d7dbe286fdea0a8810b529fc163
Prashant Khanal <prashantkhanal32@gmail.com>
2024-06-07 20:13:06 +0545

    fix: video lenght

--- src/components/web-cam-face-detection/index.tsx
+++ src/components/web-cam-face-detection/index.tsx
@@ -53,9 +53,9 @@ const WebcamDemo: React.FC<IProps> = ({
       const options: RecordRTC.Options = {
         type: 'video',
         mimeType: 'video/webm;codecs=vp8',
-        bitsPerSecond: 832 * 1024,
-        audioBitsPerSecond: 32 * 1024,
-        videoBitsPerSecond: 800 * 1024,
+        bitsPerSecond: 512 * 1024,
+        audioBitsPerSecond: 16 * 1024,
+        videoBitsPerSecond: 496 * 1024,
       };
 
       try {
