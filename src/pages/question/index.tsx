b09ca1fbe423a994e283057d2f1f583619a708d1
Prashant Khanal <prashantkhanal32@gmail.com>
2024-06-08 23:31:31 +0545

    fix: video repsonse convert

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -95,6 +95,7 @@ const Question: React.FC = () => {
               }),
               headers: {
                 'content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
               },
             }
           );
